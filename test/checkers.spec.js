f12f29487f255ee05e0f3ef230efe203fcb64be7
Joey Wilhelm <tarkatronic@gmail.com>
2021-01-29 14:48:41 -0700

    First pass testing most of the checkers

--- test/checkers.spec.js
+++ test/checkers.spec.js
@@ -22,4 +22,49 @@ describe('Checkers', function () {
       assume(result).eqls(['ford']);
     });
   });
+
+  describe('findNewMembers', function () {
+    it('finds members not yet in the org', function () {
+      const result = checkers.findNewMembers(
+        ['zaphod', 'trillian', 'marvin', 'ford'],
+        ['zaphod', 'trillian', 'marvin']
+      );
+      assume(result).eqls(['ford']);
+    });
+  });
+
+  describe('findDemotions', function () {
+    it('finds people who should not be admins', function () {
+      const result = checkers.findDemotions(
+        { zaphod: 'ADMIN', trillian: 'ADMIN', marvin: 'MEMBER', ford: 'MEMBER' },
+        { zaphod: { role: 'ADMIN' }, trillian: { role: 'ADMIN' }, marvin: { role: 'MEMBER' }, ford: { role: 'ADMIN' } }
+      );
+      assume(result).eqls(['ford']);
+    });
+  });
+
+  describe('findPromotions', function () {
+    it('finds people who should be admins', function () {
+      const result = checkers.findPromotions(
+        { zaphod: 'ADMIN', trillian: 'ADMIN', marvin: 'MEMBER', ford: 'MEMBER' },
+        { zaphod: { role: 'MEMBER' }, trillian: { role: 'ADMIN' }, marvin: { role: 'MEMBER' }, ford: { role: 'MEMBER' } }
+      );
+      assume(result).eqls(['zaphod']);
+    });
+  });
+
+  describe('validateTwoFactor', function () {
+    it('finds people somehow not using 2fa', function () {
+      const result = checkers.validateTwoFactor(
+        {
+          zaphod: { twoFactorAuth: true },
+          trillian: { twoFactorAuth: true },
+          marvin: { twoFactorAuth: true },
+          ford: { twoFactorAuth: false }
+        }
+      );
+      assume(result).eqls(['ford']);
+    });
+  });
+
 });
